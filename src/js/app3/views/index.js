bccd19568fbe0867706452a05e25ba324d3043e7
JIuctoyxuu <antonduzhnov.90@gmail.com>
2014-11-19 17:58:17 +0300

    tmp commit for task3
    problem: need include handlebars in html, include Handlebars -> kill
    Handlebars.templates

--- src/js/app3/views/index.js
+++ src/js/app3/views/index.js
@@ -1,12 +1,22 @@
-define(['jquery', 'handlebars', 'text!templates/task3.html', 'helpers'], function ($, Handlebars, template) {
-
+define(['jquery', 'handlebars', 'templates/compiled/task3', 'helpers'], function ($, Handlebars) {
+	function plurality() {
+		$( "#container li" ).each(function() {
+		var count = $(this).data("count")
+		var item = $(this).data("item")
+		if(parseInt(count)>1 && item.slice(-1)!=="s")
+			$(this).append("s");
+		})
+	} // not work
 
 
 	return {
 		render: function (allItems) {
-			var compiled = Handlebars.compile(template);
+			//var html = temp(allItems);
+			Handlebars.templates.task3({"f":"s"})
+			/*var compiled = Handlebars.compile('<div id="container">{{#each items}}{{#list @key this}}{{/list}}{{/each}}</div>');
 			$("#container").remove();
-			$("#wrap").append(compiled(allItems));
+			$("#wrap").append(compiled(allItems));*/
+			console.log(Handlebars)
 		}
 	};
 });
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,15 +1,14 @@
-define(['handlebars'], function ( Handlebars ){
+define(['handlebars'], function (Handlebars) {
 
 	var list = 	Handlebars.registerHelper('list', function(type, items) {
 		var out = "<ul>" + type;
 		$.each(items, function(index, value) {
-			out += "<li>" + index +": "+ value + "</li>";
+			out += "<li data-item="+ index +" data-count="+ value +" >" + index +": "+ value + "</li>";
 		});
 		out += "</ul>";
 		return out;
 	});
-
 	return {
-		list: list,
+		list: list
 	};
 });
